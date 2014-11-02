0e5d8662baec0b8255b97ce1eea0ed93db9968cb
doc1oo <doc100@gmail.com>
2014-11-03 00:42:42 +0900

    arrange

--- src/apps/myApps/pgDw/src/main.cpp
+++ src/apps/myApps/pgDw/src/main.cpp
@@ -3,12 +3,9 @@
 
 //--------------------------------------------------------------
 int main(){
-	//ofAppGlutWindow window; // create a window
-    
-    //window.setGlutDisplayString("rgb alpha double samples>=4 depth"); // add for antialiasing
-    //window.setGlutDisplayString("rgba double samples>=4");
-    
+   
 	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
-	ofSetupOpenGL(1200, 700, OF_WINDOW);
-	ofRunApp(new testApp()); // start the app
+	ofSetupOpenGL(1280, 720, OF_WINDOW);
+	ofRunApp(new testApp());
+
 }
